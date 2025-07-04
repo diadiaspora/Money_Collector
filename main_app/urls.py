232946d08e41475afc923f2204e284525d5ad06f
diadiaspora <dia.diaspora@gmail.com>
2025-07-04 11:26:25 -0400

    money collector home and index

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views 
+from . import views
 
-urlpatterns =[
-    path('', views.home, name='home'),
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('accounts/', views.account_index, name='account-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class Account:
+  def __init__(self, bank, type, country, currency):
+    self.bank = bank
+    self.type = type
+    self.country = country
+    self.currency = currency
+
+accounts = [
+    Account('JP Morgan Chase', 'checking', 'United States', 'USD'),
+    Account('Bank of America', 'savings', 'United States', 'USD'),
+    Account('Santander', 'checking', 'Spain', 'EUR'),
+    Account('Banco Popular', 'checking', 'A4gentina', 'ARS')
+]
+
+
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello Money</h1>')
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def account_index(request):
+  return render(request, 'accounts/index.html', {'accounts': accounts})
