932248d6bd3f252f44afcf0f6bfe5fd39a8d4862
diadiaspora <dia.diaspora@gmail.com>
2025-07-08 17:00:11 -0400

    ptogress

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    bank = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    country = models.CharField(max_length=250)
+    currency = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.bank} ({self.id})"
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-class Account:
-  def __init__(self, bank, type, country, currency):
-    self.bank = bank
-    self.type = type
-    self.country = country
-    self.currency = currency
+# class Account:
+#   def __init__(self, bank, type, country, currency):
+#     self.bank = bank
+#     self.type = type
+#     self.country = country
+#     self.currency = currency
 
-accounts = [
-    Account('JP Morgan Chase', 'checking', 'United States', 'USD'),
-    Account('Bank of America', 'savings', 'United States', 'USD'),
-    Account('Santander', 'checking', 'Spain', 'EUR'),
-    Account('Banco Popular', 'checking', 'A4gentina', 'ARS')
-]
+# accounts = [
+#     Account('JP Morgan Chase', 'checking', 'United States', 'USD'),
+#     Account('Bank of America', 'savings', 'United States', 'USD'),
+#     Account('Santander', 'checking', 'Spain', 'EUR'),
+#     Account('Banco Popular', 'checking', 'A4gentina', 'ARS')
+# ]
 
 
 # Create your views here.
